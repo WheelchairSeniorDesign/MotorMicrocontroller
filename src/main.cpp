c3ec8c6a8b88c233c0494ce04781800f25e6b651
Eren Tekbas <tekbasa@lafayette.edu>
2024-12-05 18:38:46 -0500

    using struc for reference speed retrieval

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+/* Eren Tekbas for ECE Senior Design 2025 
+MicroController Handling Reference Speed
+This code is for the microcontroller to retrieve the target speed from the onboard 
+computer and send it to the motor controller after adjusting it. 
+It will also read the speed of the motor and send it to the onboard computer.
+*/
+
 #include <Arduino.h>
 #include <Adafruit_MCP4725.h>
 
@@ -13,18 +20,24 @@ int enablePin = 11;
 int speedPin = 4;
 int refSpeedPin = 2;
 int motorSpeedPin = 22;
+int dacClock;
 
-int dacClock; //variables to be used in the code
+
+//variables to be used in the code
 int brake;
-int directionL;
-int directionR;
-int enable;
-int speed;
-int refSpeed;
-int motorSpeed;
+bool directionL; // direction for motor controller LEFT
+bool directionR; // direction for motor controller RIGHT
+bool enable; // enable for motor controller
+int speed; //
+int motorSpeed; // value read from the motor speed sensor
 
+struc refSpeed{
+    int speedR;
+    int speedL;
+    }
 
 void setup(){
+    refSpeed joystickSpeed{};
     Serial.begin(115200);
     dacA.begin(0x62);
     dacB.begin(0x63);
@@ -34,10 +47,13 @@ void setup(){
     pinMode(directionRPin,OUTPUT);
     pinMode(brakePin,OUTPUT);
     pinMode(refSpeedPin,INPUT);
+    pinMode(motorSpeedPin,INPUT);
+
 }
 
 void loop(){
-    refSpeed = digitalRead(refSpeedPin);
+    joystickSpeed{} = digitalRead(refSpeedPin);
+    joystickSpeed.speedR = ;
     if(refSpeed > -1 ^^ refSpeed < 1){
       brake = 0;
       enable = 0;
